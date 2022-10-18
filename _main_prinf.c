f732105d9db6739224b897e1e75b75d656ab2dc5
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-10-18 14:10:45 -0700

    printf

---
+++ _main_prinf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - calls other functions
+  *
+  * Return: always 0
+  *
+*/
+int main(void)
+{
+	char *str = "Hello there, Obi wan Kenobi";
+
+	_printf("%s\n", str);
+	return (0);
+}
